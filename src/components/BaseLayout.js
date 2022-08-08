29f73336f5a69d315c27cecc94112db78052f270
ellerman <106990217+ellerman4@users.noreply.github.com>
2022-08-08 12:57:02 -0500

    updated footer

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -31,7 +31,8 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>template created with &hearts; by <a href={'https://paytonpierce.dev'}>Payton Pierce</a></p>
+                  <p>template created with &hearts; by <a href={'https://paytonpierce.dev'} target="_blank" rel="noopener noreferrer">Payton Pierce</a></p>
+                  <p>expanded by <a href={'https://github.com/ellerman4'} target="_blank" rel="noopener noreferrer">me</a></p>
                   <p>&copy; 2022</p>
                </Box>
             </Grid>
